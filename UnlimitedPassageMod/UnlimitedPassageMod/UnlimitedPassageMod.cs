09d7be26b3e78c9f1662875c715d3e711c3ccaee
sudobeans <duryesch000@bellinghamschools.org>
2022-03-14 20:20:46 -0700

    add base files

---
+++ UnlimitedPassageMod/UnlimitedPassageMod/UnlimitedPassageMod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+
+namespace UnlimitedPassageMod
+{
+    [BepInPlugin("author.unlimited_passage_mod", "UnlimitedPassageMod", "0.1.0")]	// (GUID, mod name, mod version)
+    public class UnlimitedPassageMod : BaseUnityPlugin
+    {
+    }
+}
